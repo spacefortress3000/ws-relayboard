104e088a926a92b968ab76b6c30f3b14d0e7888e
spacefortress3000 <61345844+spacefortress3000@users.noreply.github.com>
2021-11-06 23:18:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from gpiozero import *
 import keyboard
 import time
 
+print('Starting switch...')
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -62,13 +64,13 @@ keyboard.add_hotkey(ch8[1], lambda: switch(ch8))
 def switch(channel):
 	if channel[2] == 'True':
 		channel[0].on()
-		print(channel[0], "ON")
+		print(channel[0], 'ON')
 		time.sleep(channel[3]/1000)
 		channel[0].off()
-		print(channel[0], "OFF")
+		print(channel[0], 'OFF')
 
 	else:
 		channel[0].toggle()
-		print(channel[0], "TOGGLE")
+		print(channel[0], 'TOGGLE')
 
 keyboard.wait()
