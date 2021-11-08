53a26edb18bb565da7ba1634400095b3eda8c6dc
spacefortress3000 <61345844+spacefortress3000@users.noreply.github.com>
2021-11-08 15:16:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,6 +60,18 @@ channels = [ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8]
 for channel in channels:
 	channel[0].off()
 
+def switch(channel):
+	if channel[2] == 'True':
+		channel[0].on()
+		print(channel[0], 'ON')
+		time.sleep(channel[3]/1000)
+		channel[0].off()
+		print(channel[0], 'OFF')
+
+	else:
+		channel[0].toggle()
+		print(channel[0], 'TOGGLE')
+
 def wait():
         keyboard.wait()
 
